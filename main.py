7f475d2d7a19f7126102b7b5e1fe61f1b6d20c36
edwardrico <120380332+edwardrico@users.noreply.github.com>
2024-02-06 16:55:22 +0100

    correction

--- main.py
+++ main.py
@@ -1,39 +1,38 @@
 cm = 2.54
 pouce = 0.394
+
 def converseur():
     while True:
-
-        print("Option : ")
-        print("1- Souhaitez vous convertir pouce a cm ?")
-        print("2- Souhaitez vous convertir cm a pouce ?")
-        print("3- Termmine ")
+        print("Options : ")
+        print("1- Souhaitez-vous convertir pouce en cm ?")
+        print("2- Souhaitez-vous convertir cm en pouce ?")
+        print("3- Terminé ")
         try:
-            option = int(input(" choisi entre les option : /1/2/3/ : "))
+            option = int(input("Choisissez parmi les options : /1/2/3/ : "))
             if 1 <= option <= 3:
                 if option == 1:
                     try:
-                        choix = float(input("Rentre le nombre de pouce que vous souhaitez convertir : "))
-                        print(f"{choix} pouce equivaut a {round(choix * cm)} cm ")
+                        choix = float(input("Entrez le nombre de pouces que vous souhaitez convertir : "))
+                        print(f"{choix} pouces équivaut à {round(choix * cm, 2)} cm ")
                         break
                     except ValueError:
-                        print("Erreur vous doit rentre un nombre pour vos valeur ")
+                        print("Erreur, vous devez entrer un nombre pour vos valeurs ")
 
                 if option == 2:
                     try:
-                        choix1 = float(input("Rentre le nombre de cm que vous souhaitez convertir : "))
-                        print(f"{choix1} cm equivaut a {round(choix1 * pouce)} pouce  ")
+                        choix1 = float(input("Entrez le nombre de cm que vous souhaitez convertir : "))
+                        print(f"{choix1} cm équivaut à {round(choix1 * pouce, 2)} pouces  ")
                         break
                     except ValueError:
-                        print("Erreur vous doit rentre un nombre pour vos valeur ")
+                        print("Erreur, vous devez entrer un nombre pour vos valeurs ")
 
                 elif option == 3:
-                    print("Merci au revoir !")
+                    print("Merci, au revoir !")
                     break
             else:
-                print("Option invaide choisir parmis les option valide.")
+                print("Option invalide. Veuillez choisir parmi les options valides.")
         except ValueError:
-            print("Erreur vous doit rentre un nombre pour votre option!")
+            print("Erreur, vous devez entrer un nombre pour votre option!")
         print()
-
 
 converseur()
